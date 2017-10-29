e7218c142c73a8d933509dc4b5764d533fa3d1a6
Adam Kapelner <rochlevi@gmail.com>
2017-10-29 10:57:26 -0400

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,22 @@
+
 def alphabetize(arr)
-  # code here
-end
+	esperatano = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz ".split(//)
+	sorted_arr = []
+	longest = arr.max_by(&:length).length
+	i = longest 
+	while i > 0
+		sorted_arr = arr.sort_by do |phrase| 
+			if phrase[i - 1] != nil
+				esperatano.index(phrase[i - 1])
+			else
+				0
+			end
+		end
+		i -= 1
+		arr = sorted_arr
+	end
+	sorted_arr
+end	
+
+
+
